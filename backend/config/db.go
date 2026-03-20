0342025c4aee106d6a4c1b7858eb5c9f3bb8375e
ManujaLakshitha <rajinda1205@gmail.com>
2026-03-20 22:23:32 +0530

    Improve gitignore for frontend and backend environment

--- backend/config/db.go
+++ backend/config/db.go
@@ -3,25 +3,42 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	connStr := "user=postgres password=Manu@20020512 dbname=clinic sslmode=disable"
-	var err error
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	name := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	connStr := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		user, pass, name, host, port,
+	)
+
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = DB.Ping()
-	if err != nil {
+	if err := DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Connected to database!")
+	log.Println("Connected to database securely!")
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module clinic-system
 go 1.26.1
 
 require github.com/lib/pq v1.11.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
